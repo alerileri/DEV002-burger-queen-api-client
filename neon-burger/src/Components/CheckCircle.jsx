c70df543affab07a7bbeaa072e5485e06c2065b8
alerileri <ale.ortmed@gmail.com>
2023-03-18 23:24:55 -0600

    Icons importados a componentes

---
+++ neon-burger/src/Components/CheckCircle.jsx
@@ -0,0 +1,11 @@
+import { CheckCircleIcon } from '@heroicons/react/24'
+
+function CircleStatus () {
+    return (
+        <div>
+            <CheckCircleIcon className='' />
+            </div>
+    )
+}
+
+export default CircleStatus
