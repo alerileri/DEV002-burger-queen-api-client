57d9cbac15d869c6484aa56e656fc9b2fe3a7ac4
alerileri <ale.ortmed@gmail.com>
2023-03-21 11:42:13 -0600

    Componentes reutilizables clasificados

--- neon-burger/src/Pages/Login.jsx
+++ neon-burger/src/Pages/Login.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 const LoginPage = () => {
     return (
         <>
+        <LogoNB />
         <nav style={{ display: "flex", justifyContent: "space-around" }}>
           <Link to="/admin">Admin</Link>
           <Link to="/chef">Chef</Link>
