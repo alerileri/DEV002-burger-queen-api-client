81a582d5acaf6b5715373db2a3f206360109c014
alerileri <ale.ortmed@gmail.com>
2023-03-22 03:21:56 -0600

    Archivo Router añadido a la rama

--- neon-burger/src/Pages/AdminPage.jsx
+++ neon-burger/src/Pages/AdminPage.jsx
@@ -11,3 +11,4 @@ function AdminPage() {
 }
 
 export default AdminPage
+
---
+++ neon-burger/src/Router.jsx
@@ -0,0 +1,24 @@
+//este archivo sustituirá al archivo App.js en el index.js de nuestra aplicación, en él haremos la configuración y la gestión de nuestras rutas:
+
+// si queremos proveer a toda nuestra aplicación de rutas tenemos que posicionar al componente BrowserRouter como el componente padre de todas nuestras rutas
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const Router = () => {
+  const LoginPage = () => <h1>Login</h1>
+  const AdminPage = () => <h1>Admin</h1>;
+  const ChefPage = () => <h1>Chef</h1>;
+  const WaiterPage = () => <h1>Waiter</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="admin" element={<AdminPage />}></Route>
+          <Route path="chef" element={<ChefPage />}></Route>
+          <Route path="waiter" element={<WaiterPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+export default Router;
