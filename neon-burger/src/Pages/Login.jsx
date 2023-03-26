433a33def6585fc61c38b921dab8fa56dbb7e8c2
alerileri <ale.ortmed@gmail.com>
2023-03-26 12:55:35 -0600

    Login con username

--- neon-burger/src/Pages/Login.jsx
+++ neon-burger/src/Pages/Login.jsx
@@ -1,13 +1,29 @@
 //import React from "react";
+import { useState } from 'react';
 import Logo from '../Components/LogoNB'
-import LoginForm from '../Components/Reusable/LoginForm';
 import './login.css'
 
-function LoginPage () {
+function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] =useState(false);
+
+    const handleLogin = () => {
+        props.history.push('/admin');
+
+    }
     return (
         <div className='login'>
-        <Logo className='neon-logo'/>
-        <LoginForm className='login-form' />
+            <Logo className='neon-logo' />
+            <form className='login-form'>
+                <label className='label-form'>Email</label>
+                <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='user@neonburger.com'></input>
+                <label>Password</label>
+                <input type='password' placeholder='••••••••' id='password'></input>
+                {error && <div className='error'>{error}</div>}
+                <button type='submit' value={loading ? 'Loading...' : 'Login'} disabled={loading} onclick={handleLogin}>Login</button>
+            </form>
         </div>
     );
 };
