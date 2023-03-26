f78b5e00bb73091785a59dfb6a420ac3a4ee6d94
alerileri <ale.ortmed@gmail.com>
2023-03-26 13:48:17 -0600

    Avance función login

--- neon-burger/src/Pages/Login.jsx
+++ neon-burger/src/Pages/Login.jsx
@@ -4,10 +4,10 @@ import Logo from '../Components/LogoNB'
 import './login.css'
 
 function LoginPage() {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
-    const [loading, setLoading] =useState(false);
+    const [loading, setLoading] = useState(false);
 
     const handleLogin = () => {
         props.history.push('/admin');
@@ -18,9 +18,21 @@ function LoginPage() {
             <Logo className='neon-logo' />
             <form className='login-form'>
                 <label className='label-form'>Email</label>
-                <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='user@neonburger.com'></input>
+                <input
+                    type='text'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder='user@neonburger.com'>
+                </input>
                 <label>Password</label>
-                <input type='password' placeholder='••••••••' id='password'></input>
+                <input
+                    type='password'
+                    placeholder='••••••••'
+                    id='password'
+                    value={password}
+                    onChange={e =>setPassword(e.target.value)}
+                    >
+                </input>
                 {error && <div className='error'>{error}</div>}
                 <button type='submit' value={loading ? 'Loading...' : 'Login'} disabled={loading} onclick={handleLogin}>Login</button>
             </form>
